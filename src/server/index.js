e4721c67218d46a947e6964f7d14bb99293d4ca5
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2020-11-12 18:12:48 +0100

    feat: Create naive api call

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,18 @@
 var path = require('path');
 const express = require('express');
+const axios = require('axios');
+const querystring = require('querystring');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const mockAPIResponse = require('./mockAPI.js');
 const dotenv = require('dotenv');
 dotenv.config();
 const apiKey = process.env.API_KEY;
+const baseURL = 'https://api.meaningcloud.com/sentiment-2.1';
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('dist'));
 
@@ -24,4 +30,21 @@ app.listen(8081, function () {
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse);
+});
+
+app.post('/analyze', async (req, res) => {
+    const data = req.body;
+    const text = data.userInput;
+    const lang = data.lang;
+    const url = `${baseURL}?key=${apiKey}&lang=${lang}&txt=${querystring.escape(
+        text
+    )}`;
+    axios
+        .post(url)
+        .then((response) => {
+            res.send(response.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 });
