edce210a8e894b6f937d10df922616a6f983781d
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2020-11-12 16:05:15 +0100

    chore: Set up environment variables

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,9 @@
 var path = require('path');
 const express = require('express');
 const mockAPIResponse = require('./mockAPI.js');
+const dotenv = require('dotenv');
+dotenv.config();
+const apiKey = process.env.API_KEY;
 
 const app = express();
 
