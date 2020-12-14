1fee2fe131b4dcca27ceb6d8810956b1d31e1893
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2020-12-14 17:52:22 +0100

    feat: Add error feedback in case of invalid input

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -5,28 +5,39 @@ import { isValidText } from './textValidator';
 function handleSubmit(event) {
     event.preventDefault();
 
-    // check what text was put into the form field
-    const formText = document.getElementById('name').value.trim();
-    const isURL = isValidURL(formText);
-    const isText = isValidText(formText);
+    const formField = document.getElementById('name');
+    const errorMessage = document.getElementById('errorMessage');
 
-    fetch('http://localhost:8081/analyze', {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            userInput: formText,
-            lang: 'en',
-            url: isURL,
-        }),
-    })
-        .then((res) => res.json())
-        .then(function (res) {
-            res['userInput'] = formText;
-            updateUI(res);
-        });
+    // clear the error message in case it was visible
+    errorMessage.textContent = '';
+    // check what text was put into the form field
+    const formText = formField.value.trim();
+    const isURL = isValidURL(formText);
+
+    if (isURL || isValidText(formText)) {
+        fetch('http://localhost:8081/analyze', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                userInput: formText,
+                lang: 'en',
+                url: isURL,
+            }),
+        })
+            .then((res) => res.json())
+            .then(function (res) {
+                res['userInput'] = formText;
+                updateUI(res);
+            });
+    } else {
+        // input doesn't pass validation: clear input form and show error message
+        formField.value = '';
+        errorMessage.textContent =
+            'Please enter a valid URL or a valid English text';
+    }
 }
 
 export { handleSubmit };
