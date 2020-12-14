e4a9f74db4eaed5f1eacd75131cae1ee9285fdd1
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2020-12-14 17:52:56 +0100

    test: Add tests to handleSubmit

--- __test__/handleSubmit.test.js
+++ __test__/handleSubmit.test.js
@@ -17,6 +17,7 @@ import { handleSubmit } from '../src/client/js/formHandler';
 describe('Testing the submit functionality', () => {
     beforeEach(() => {
         document.body.innerHTML = `<div>
+            <div id="errorMessage"></div>
             <form>
                 <input type="text" value="text" id="name">
                 <input type="submit" value="submit" id="submit">
@@ -31,6 +32,26 @@ describe('Testing the submit functionality', () => {
             .addEventListener('click', handleSubmit);
         fetch.mockClear();
     });
+    test('Testing that handleSubmit clears the error message if the input is valid', () => {
+        const errorMessage = document.getElementById('errorMessage');
+        errorMessage.textContent =
+            'Please enter a valid URL or a valid English text';
+        isValidURL.mockImplementationOnce((value) => true);
+        document.getElementById('submit').click();
+        expect(errorMessage.textContent).toBe('');
+    });
+    test('Testing that handleSubmit sets the error message if the input is invalid', () => {
+        const errorMessage = document.getElementById('errorMessage');
+        document.getElementById('submit').click();
+        expect(errorMessage.textContent).toBe(
+            'Please enter a valid URL or a valid English text'
+        );
+    });
+    test('Testing that handleSubmit clears the input field if the input is invalid', () => {
+        const inputField = document.getElementById('name');
+        document.getElementById('submit').click();
+        expect(inputField.value).toBe('');
+    });
     test('Testing that handleSubmit calls isValidURL with the correct argument', () => {
         document.getElementById('submit').click();
         expect(isValidURL).toHaveBeenCalledWith('text');
@@ -40,10 +61,12 @@ describe('Testing the submit functionality', () => {
         expect(isValidText).toHaveBeenCalledWith('text');
     });
     test('Testing that handleSubmit calls fetch', () => {
+        isValidURL.mockImplementationOnce((value) => true);
         document.getElementById('submit').click();
         expect(fetch).toHaveBeenCalled();
     });
     test('Testing that handleSubmit calls updateUI', () => {
+        isValidURL.mockImplementationOnce((value) => true);
         document.getElementById('submit').click();
         expect(updateUI).toHaveBeenCalled();
     });
