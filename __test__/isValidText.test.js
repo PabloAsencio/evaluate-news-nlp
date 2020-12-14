54421967c2bdd538dc5c58b380970de5baa09056
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2020-12-14 17:21:55 +0100

    test: Add additional tests for isValidText

--- __test__/isValidText.test.js
+++ __test__/isValidText.test.js
@@ -4,6 +4,19 @@ describe('Testing the text validation functionality', () => {
     test('Testing a valid text', () => {
         expect(isValidText('This is some text')).toBe(true);
     });
+    test('Test a text including punctuation', () => {
+        expect(
+            isValidText(
+                'Text including punctuation, such as this one, should be valid as well.'
+            )
+        ).toBe(true);
+    });
+    test('Testing text with an apostrophe in it', () => {
+        expect(isValidText("Text like this shouldn't be rejected")).toBe(true);
+    });
+    test('Testing text with numbers', () => {
+        expect(isValidText('$100,000 is 50.0% of $200,000.00')).toBe(true);
+    });
     test('Testing a text with some js in it', () => {
         expect(isValidText('<script>alert("XSS");</script>')).toBe(false);
     });
